c5e53c29bcacac4810e6bd7d0603074fc8f7426a
Luis Rivera-Wong <lriverawong@outlook.com>
2018-03-16 17:41:49 -0400

    more code clean

--- alphabeta.py
+++ alphabeta.py
@@ -1,6 +1,7 @@
 import re
 import math
 
+
 def alphabeta():
     input_filename = "alphabeta.txt"
     file = open(input_filename, "r")
@@ -22,26 +23,19 @@ def alphabeta_reader(line):
     temp_base = {}
     root = None
     temp_input01 = line.split(' ')
-    #print(temp_input01)
     t_part01 = temp_input01[0]
     t_part01 = t_part01[2:-2].split('),(')
-    #print(t_part01)
     t_part02 = temp_input01[1]
     t_part02 = re.search('{(.+?)}', t_part02).group(1)
-    #print(t_part02)
     t_part02 = t_part02[1:-1].split('),(')
-    #print(t_part02)
     
     # building base
     for j in t_part01:
         t_node = j.split(',')
         t_letter = t_node[0]
-        #print(t_letter)
         t_minmax_letter = t_node[1][1] == 'I'
-        #print(t_minmax_letter)
         temp_node = Node(t_letter, t_minmax_letter)
         temp_base[t_letter] = temp_node
-        #print("testing the dic = ", temp_base[t_letter].min)
     
     # building children
     if len(t_part02) > 0:
@@ -49,12 +43,9 @@ def alphabeta_reader(line):
     for k in t_part02:
         t_node = k.split(',')
         t_fletter = t_node[0]
-        #print(t_fletter)
         t_sletter = t_node[1]
-        #print(t_sletter)
         some_node = temp_base[t_fletter]
         if (t_sletter in temp_base):
-            #print(temp_base[t_sletter])
             some_node.childrenSetter(temp_base[t_sletter])
         else:
             # assume that its numeric
@@ -63,28 +54,16 @@ def alphabeta_reader(line):
                 del chars[len(chars) - 1]
             t_sletter = ''.join(chars)
             some_node.valueSetter(int(t_sletter))
-        
-    # for x in temp_base:
-    #     print('----------')
-    #     print("Letter = ", temp_base[x].letter)
-    #     print("Min Bool = ", temp_base[x].min)
-    #     print("Value length = ", len(temp_base[x].values))
-    #     print("Values: ")
-    #     for y in temp_base[x].values:
-    #         print("\t", y)
-    #     print("Children length = ", len(temp_base[x].children))
                 
     return (temp_base, root)
 
-class Node:
-    node_count = 0
 
+class Node:
     def __init__(self, letter, minmax, value=-1):
         self.letter = letter
         self.min = minmax
         self.values = []
         self.children = []
-        Node.node_count += 1
     
     def valueSetter(self, value):
         self.values.append(value)
@@ -136,13 +115,6 @@ class Node:
                         print("\nNode: %s" % self.letter + "\nAlpha %s" % a + " is better than beta %s" % b)
                         return (a, examined)
                 return (a, examined)
-
-
-
-    def displayCount(self):
-     print ("Total Nodes = %d" % Node.node_count)
-
-    def displayNode(self):
-      print ("letter: ", self.letter,", minmax: ", self.minmax, " value: ", self.value)
+        
 
 alphabeta()
